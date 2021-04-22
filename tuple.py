36f65c0db72233ca449d0b064ceaca7be65f30e5
Rohitha Koduri <rohithakoduri@gmail.com>
2021-04-22 21:56:31 +0530

    list of tuples into dictionary

---
+++ tuple.py
@@ -0,0 +1,7 @@
+def listtodict(A, di): 
+   di = dict(A) 
+   return di 
+
+A = [("Adwaita", 5), ("Aadrika", 5), ("Babai", 37), ("Mona", 7), ("Sanj", 25), ("Sakya", 30)] 
+di = {} 
+print ("The Dictionary Is ::>",listtodict(A, di))
