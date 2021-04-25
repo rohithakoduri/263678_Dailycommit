038aa78a80f1f4170ae952da39d9d939ddc32a0e
Rohitha Koduri <rohithakoduri@gmail.com>
2021-04-25 10:24:24 +0530

    using function

---
+++ function.py
@@ -0,0 +1,5 @@
+def my_function(fname):
+    print('Hi! '+fname)
+
+my_function('Tom')
+my_function('Jim')
--- list.py
+++ list.py
@@ -1,7 +1,7 @@
 def find_largest(list1):
     l=len(list1)
     list1.sort()
-    printf("Second largest:",list1[1])
+    print("Second largest:",list1[1])
 
 list1=[2,6,3,8,5,4,9,1]
 largest=find_largest(list1)
